5aec0e0e93595e2994fc9e8975b9369241dec5b2
KevinLCai <65425655+KevinLCai@users.noreply.github.com>
2020-09-07 19:54:13 +0100

    Add files via upload
    Updated

--- solver.py
+++ solver.py
@@ -1,4 +1,4 @@
-board = [
+unsolvedBoard = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
@@ -10,74 +10,74 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def solve(bo):
+def solve(board):
     """Recursive solution to matrix"""
-    find = find_empty(bo)
-    if not find:
+    found = find_empty_space(board)
+    if not found:
         return True
-    else:
-        row, col = find
-
+    else: 
+        row, col = found
+        
     for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
-
-            if solve(bo):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+            
+            if solve(board):
                 return True
-
-            bo[row][col] = 0
-
+            
+            board[row][col] = 0
+            
     return False
 
-def valid(bo, num, pos):
+def valid(board, num, pos):
     """Check if the board is valid"""
     #Check row for duplicate number
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
             return False
-
+    
     #Check Column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
             return False
-
+    
     #Check Box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
-
+    
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if board[i][j] == num and (i,j) != pos:
                 return False
-
+    
     #If number jumps through all the above hoops - return True!
     return True
-
-def print_board(bo):
+            
+def print_board(board):
     """Print the board"""
-    for i in range(len(bo)):
+    for i in range(len(board)):
         if i % 3 == 0 and i !=0:
             print("- - - - - - - - - - - -")
-
-        for j in range(len(bo[0])):
+            
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end = "")
-
+                    
             if j == 8:
-                print(bo[i][j])
+                print(board[i][j])
             else:
-                print(str(bo[i][j]) + " ", end = "")
+                print(str(board[i][j]) + " ", end = "")
 
-def find_empty(bo):
+def find_empty_space(board):
     """Find the first available empty square"""
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
                 return(i, j)
     #If no blank squares - will trigger solved
     return None
-
-print_board(board)
-solve(board)
+            
+print_board(unsolvedBoard)
+solve(unsolvedBoard)
 print('\n')
-print_board(board)
+print_board(unsolvedBoard)
